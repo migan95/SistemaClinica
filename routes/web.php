b206f81d41dd9f217f9fc70e04b9426850b64a67
jomanuelrp <jomanuelrp@gmail.com>
2022-04-25 16:55:37 -0600

    Actualiza rutas
    Actualiza rutas de usuarios registrados

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
